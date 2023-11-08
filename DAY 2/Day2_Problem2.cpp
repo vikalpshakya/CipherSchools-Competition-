5d92c22bf16256b2c7e171e350c4820bc857bd47
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-11-08 15:23:56 +0530

    Day 2 Problem 2 Solved @Cipherschools - competition 4.0

--- DAY 2/Day2_Problem2.cpp
+++ DAY 2/Day2_Problem2.cpp
@@ -1,49 +1,29 @@
-     #include<bits/stdc++.h>
-     vector<int>nextSmall(vector<int>arr, int n){
-        stack<int>s;
-        s.push(-1);
-        vector<int>ans(n);
-        for(int i = n-1; i >= 0; i--){
-            int curr = arr[i];
-            while(s.top() != -1 && arr[s.top()] >= curr){
-                s.pop();
+#include<bits/stdc++.h>
+
+int largestRectangle(vector < int > & h) {
+    int n=h.size();
+    stack<int> st;
+
+    int maxr=0;
+
+    for(int i=0;i<n;i++){
+        while(!st.empty() && h[st.top()]>= h[i]){
+            int x=st.top();
+            st.pop();
+            int width=st.empty()? i:i-st.top()-1;
+            int area =h[x]*width;
+            maxr=max(maxr,area);
             }
-            ans[i] = s.top();
-            s.push(i);
-        }
-        return ans;
-    }
-    vector<int>prevSmall(vector<int>arr, int n){
-        stack<int>s;
-        s.push(-1);
-        vector<int>ans(n);
-        for(int i = 0; i < n; i++){
-            int curr = arr[i];
-            while(s.top() != -1 && arr[s.top()] >= curr){
-                s.pop();
-            }
-            ans[i] = s.top();
-            s.push(i);
-        }
-        return ans;        
+        st.push(i);
     }
 
- int largestRectangle(vector < int > & heights) {
-        int n = heights.size(); 
-        vector<int> next(n);
-        next = nextSmall(heights, n);
-
-        vector<int> prev(n);
-        prev = prevSmall(heights, n);
-        int area = INT_MIN;
-        for(int i = 0; i < n; i++){
-            int len = heights[i];
-            if(next[i] == -1)
-                next[i] = n;
-            int breath = next[i] - prev[i] -1;
-            int newArea = len * breath;
-            area = max(area, newArea); 
-        }
-        return area;
+    while(!st.empty()){
+        int x=st.top();
+        st.pop();
+        int width=st.empty()? n:n-st.top()-1;
+        int area=h[x]*width;
+        maxr=max(maxr,area);
     }
- 
+    return maxr;
+
+}
