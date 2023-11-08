e40376da05560ffcfd1d9f402239a62b569353dc
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-11-08 15:26:54 +0530

    Day2 Problem 2 Solved @Cipherschools - competition 4.0

--- DAY 2/Day2_Problem2.cpp
+++ DAY 2/Day2_Problem2.cpp
@@ -1,29 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-int largestRectangle(vector < int > & h) {
-    int n=h.size();
+int largestRectangle(vector<int>& h) {
+    int n = h.size();
     stack<int> st;
 
-    int maxr=0;
+    int maxr = 0;
 
-    for(int i=0;i<n;i++){
-        while(!st.empty() && h[st.top()]>= h[i]){
-            int x=st.top();
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && h[st.top()] >= h[i]) {
+            int x = st.top();
             st.pop();
-            int width=st.empty()? i:i-st.top()-1;
-            int area =h[x]*width;
-            maxr=max(maxr,area);
+            int width;
+            if (st.empty()) {
+                width = i;
+            } else {
+                width = i - st.top() - 1;
             }
+            int area = h[x] * width;
+            maxr = max(maxr, area);
+        }
         st.push(i);
     }
 
-    while(!st.empty()){
-        int x=st.top();
+    while (!st.empty()) {
+        int x = st.top();
         st.pop();
-        int width=st.empty()? n:n-st.top()-1;
-        int area=h[x]*width;
-        maxr=max(maxr,area);
+        int width;
+        if (st.empty()) {
+            width = n;
+        } else {
+            width = n - st.top() - 1;
+        }
+        int area = h[x] * width;
+        maxr = max(maxr, area);
     }
     return maxr;
-
 }
